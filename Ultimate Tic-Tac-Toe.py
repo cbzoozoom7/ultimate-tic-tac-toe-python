62b3359f241682177eb1ec92e95b31a11adc7400
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-03-09 00:21:03 -0600

    Add files via upload

---
+++ Ultimate Tic-Tac-Toe.py
@@ -0,0 +1,372 @@
+#Works best as Python 2 in Enthrought Canopy
+one = " "
+oneOne = " "
+oneTwo = " "
+oneThree = " "
+oneFour = " "
+oneFive = " "
+oneSix = " "
+oneSeven = " "
+oneEight = " "
+oneNine = " "
+two = " "
+twoOne = " "
+twoTwo = " "
+twoThree = " "
+twoFour = " "
+twoFive = " "
+twoSix = " "
+twoSeven = " "
+twoEight = " "
+twoNine = " "
+three = " "
+threeOne = " "
+threeTwo = " "
+threeThree = " "
+threeFour = " "
+threeFive = " "
+threeSix = " "
+threeSeven = " "
+threeEight = " "
+threeNine = " "
+four = " "
+fourOne = " "
+fourTwo = " "
+fourThree = " "
+fourFour = " "
+fourFive = " "
+fourSix = " "
+fourSeven = " "
+fourEight = " "
+fourNine = " "
+five = " "
+fiveOne = " "
+fiveTwo = " "
+fiveThree = " "
+fiveFour = " "
+fiveFive = " "
+fiveSix = " "
+fiveSeven = " "
+fiveEight = " "
+fiveNine = " "
+six = " "
+sixOne = " "
+sixTwo = " "
+sixThree = " "
+sixFour = " "
+sixFive = " "
+sixSix = " "
+sixSeven = " "
+sixEight = " "
+sixNine = " "
+seven = " "
+sevenOne = " "
+sevenTwo = " "
+sevenThree = " "
+sevenFour = " "
+sevenFive = " "
+sevenSix = " "
+sevenSeven = " "
+sevenEight = " "
+sevenNine = " "
+eight = " "
+eightOne = " "
+eightTwo = " "
+eightThree = " "
+eightFour = " "
+eightFive = " "
+eightSix = " "
+eightSeven = " "
+eightEight = " "
+eightNine = " "
+nine = " "
+nineOne = " "
+nineTwo = " "
+nineThree = " "
+nineFour = " "
+nineFive = " "
+nineSix = " "
+nineSeven = " "
+nineEight = " "
+nineNine = " "
+x = 0
+o = 0
+win = 0
+freedom = 1
+while one == " " or two == " " or three == " " or four == " " or five == " " or six == " " or seven == " " or eight == " " or nine == " ":
+    if o <= x:
+        if oneOne == oneTwo == oneThree != " " or oneFour == oneFive == oneSix != " " or oneSeven == oneEight == oneNine != " " or oneOne == oneFour == oneSeven != " " or oneTwo == oneFive == oneEight != " " or oneThree == oneSix == oneNine != " " or oneOne == oneFive == oneNine != " " or oneThree == oneFive == oneSeven != " ":
+            print "Player 2 wins board 1"
+            one = "X"
+            oneOne = "X"
+            oneTwo = "X"
+            oneThree = "X"
+            oneFour = "X"
+            oneFive = "X"
+            oneSix = "X"
+            oneSeven = "X"
+            oneEight = "X"
+            oneNine = "X"
+        elif twoOne == twoTwo == twoThree != " " or twoFour == twoFive == twoSix != " " or twoSeven == twoEight == twoNine != " " or twoOne == twoFour == twoSeven != " " or twoTwo == twoFive == twoEight != " " or twoThree == twoSix == twoNine != " " or twoOne == twoFive == twoNine != " " or twoThree == twoFive == twoSeven != " ":
+            print "Player 2 wins board 2"
+            two = "X"
+            twoOne = "X"
+            twoTwo = "X"
+            twoThree = "X"
+            twoFour = "X"
+            twoFive = "X"
+            twoSix = "X"
+            twoSeven = "X"
+            twoEight = "X"
+            twoNine = "X"
+        elif threeOne == threeTwo == threeThree != " " or threeFour == threeFive == threeSix != " " or threeSeven == threeEight == threeNine != " " or threeOne == threeFour == threeSeven != " " or threeTwo == threeFive == threeEight != " " or threeThree == threeSix == threeNine != " " or threeOne == threeFive == threeNine != " " or threeThree == threeFive == threeSeven != " ":
+            print "Player 2 wins board 3"
+            three = "X"
+            threeOne = "X"
+            threeTwo = "X"
+            threeThree = "X"
+            threeFour = "X"
+            threeFive = "X"
+            threeSix = "X"
+            threeSeven = "X"
+            threeEight = "X"
+            threeNine = "X"
+        elif fourOne == fourTwo == fourThree != " " or fourFour == fourFive == fourSix != " " or fourSeven == fourEight == fourNine != " " or fourOne == fourFour == fourSeven != " " or fourTwo == fourFive == fourEight != " " or fourThree == fourSix == fourNine != " " or fourOne == fourFive == fourNine != " " or fourThree == fourFive == fourSeven != " ":
+            print "Player 2 wins board 4"
+            four = "X"
+            fourOne = "X"
+            fourTwo = "X"
+            fourThree = "X"
+            fourFour = "X"
+            fourFive = "X"
+            fourSix = "X"
+            fourSeven = "X"
+            fourEight = "X"
+            fourNine = "X"
+        elif fiveOne == fiveTwo == fiveThree != " " or fiveFour == fiveFive == fiveSix != " " or fiveSeven == fiveEight == fiveNine != " " or fiveOne == fiveFour == fiveSeven != " " or fiveTwo == fiveFive == fiveEight != " " or fiveThree == fiveSix == fiveNine != " " or fiveOne == fiveFive == fiveNine != " " or fiveThree == fiveFive == fiveSeven != " ":
+            print "Player 2 wins board 5"
+            five = "X"
+            fiveOne = "X"
+            fiveTwo = "X"
+            fiveThree = "X"
+            fiveFour = "X"
+            fiveFive = "X"
+            fiveSix = "X"
+            fiveSeven = "X"
+            fiveEight = "X"
+            fiveNine = "X"
+        elif sixOne == sixTwo == sixThree != " " or sixFour == sixFive == sixSix != " " or sixSeven == sixEight == sixNine != " " or sixOne == sixFour == sixSeven != " " or sixTwo == sixFive == sixEight != " " or sixThree == sixSix == sixNine != " " or sixOne == sixFive == sixNine != " " or sixThree == sixFive == sixSeven != " ":
+            print "Player 2 wins board 6"
+            six = "X"
+            sixOne = "X"
+            sixTwo = "X"
+            sixThree = "X"
+            sixFour = "X"
+            sixFive = "X"
+            sixSix = "X"
+            sixSeven = "X"
+            sixEight = "X"
+            sixNine = "X"
+        elif sevenOne == sevenTwo == sevenThree != " " or sevenFour == sevenFive == sevenSix != " " or sevenSeven == sevenEight == sevenNine != " " or sevenOne == sevenFour == sevenSeven != " " or sevenTwo == sevenFive == sevenEight != " " or sevenThree == sevenSix == sevenNine != " " or sevenOne == sevenFive == sevenNine != " " or sevenThree == sevenFive == sevenSeven != " ":
+            print "Player 2 wins board 7"
+            seven = "X"
+            sevenOne = "X"
+            sevenTwo = "X"
+            sevenThree = "X"
+            sevenFour = "X"
+            sevenFive = "X"
+            sevenSix = "X"
+            sevenSeven = "X"
+            sevenEight = "X"
+            sevenNine = "X"
+        elif eightOne == eightTwo == eightThree != " " or eightFour == eightFive == eightSix != " " or eightSeven == eightEight == eightNine != " " or eightOne == eightFour == eightSeven != " " or eightTwo == eightFive == eightEight != " " or eightThree == eightSix == eightNine != " " or eightOne == eightFive == eightNine != " " or eightThree == eightFive == eightSeven != " ":
+            print "Player 2 wins board 8"
+            eight = "X"
+            eightOne = "X"
+            eightTwo = "X"
+            eightThree = "X"
+            eightFour = "X"
+            eightFive = "X"
+            eightSix = "X"
+            eightSeven = "X"
+            eightEight = "X"
+            eightNine = "X"
+        elif nineOne == nineTwo == nineThree != " " or nineFour == nineFive == nineSix != " " or nineSeven == nineEight == nineNine != " " or nineOne == nineFour == nineSeven != " " or nineTwo == nineFive == nineEight != " " or nineThree == nineSix == nineNine != " " or nineOne == nineFive == nineNine != " " or nineThree == nineFive == nineSeven != " ":
+            print "Player 2 wins board 9"
+            nine = "X"
+            nineOne = "X"
+            nineTwo = "X"
+            nineThree = "X"
+            nineFour = "X"
+            nineFive = "X"
+            nineSix = "X"
+            nineSeven = "X"
+            nineEight = "X"
+            nineNine = "X"
+        if one == two == three != " " or four == five == six != " " or seven == eight == nine != " " or one == four == seven != " " or two == five == eight != " " or three == six == nine != " " or one == five == nine != " " or three == five == seven != " ":
+            print "Player 2 wins."
+            one = "X"
+            two = "X"
+            three = "X"
+            four = "X"
+            five = "X"
+            six = "X"
+            seven = "X"
+            eight = "X"
+            nine = "X"
+            win = 1
+        else:
+            print "1|2|3"
+            print "-+-+-"
+            print "4|5|6"
+            print "-+-+-"
+            print "7|8|9"
+            move = input("Player 1: Make your move. ")
+            if move == 1:
+                if one == " ":
+                    one = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 2:
+                if two == " ":
+                    two = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 3:
+                if three == " ":
+                    three = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 4:
+                if four == " ":
+                    four = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 5:
+                if five == " ":
+                    five = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 6:
+                if six == " ":
+                    six = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 7:
+                if seven == " ":
+                    seven = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 8:
+                if eight == " ":
+                    eight = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            elif move == 9:
+                if nine == " ":
+                    nine = "O"
+                    o = o+1
+                else:
+                    print "That's cheating!"
+            else:
+                print "Invalid input."
+            print one, "|", two, "|", three
+            print "--+---+--"
+            print four, "|", five, "|", six
+            print "--+---+--"
+            print seven, "|", eight, "|", nine
+    
+    elif o > x:
+            if one == two == three != " " or four == five == six != " " or seven == eight == nine != " " or one == four == seven != " " or two == five == eight != " " or three == six == nine != " " or one == five == nine != " " or three == five == seven != " ":
+                print "Player 1 wins."
+                one = "O"
+                two = "O"
+                three = "O"
+                four = "O"
+                five = "O"
+                six = "O"
+                seven = "O"
+                eight = "O"
+                nine = "O"
+                win = 1
+            else:
+                print "1|2|3"
+                print "-+-+-"
+                print "4|5|6"
+                print "-+-+-"
+                print "7|8|9"
+                move = input("Player 2: Make your move. ")
+                if move == 1:
+                    if one == " ":
+                        one = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 2:
+                    if two == " ":
+                        two = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 3:
+                    if three == " ":
+                        three = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 4:
+                    if four == " ":
+                        four = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 5:
+                    if five == " ":
+                        five = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 6:
+                    if six == " ":
+                        six = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 7:
+                    if seven == " ":
+                        seven = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 8:
+                    if eight == " ":
+                        eight = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                elif move == 9:
+                    if nine == " ":
+                        nine = "X"
+                        x = x+1
+                    else:
+                        print "That's cheating!"
+                else:
+                    print "Invalid input."
+                print one, "|", two, "|", three
+                print "--+---+--"
+                print four, "|", five, "|", six
+                print "--+---+--"
+                print seven, "|", eight, "|", nine
+if win != 1:
+    if one == two == three != " " or four == five == six != " " or seven == eight == nine != " " or one == four == seven != " " or two == five == eight != " " or three == six == nine != " " or one == five == nine != " " or three == five == seven != " ":
+        print "Player 1 wins."
+    else:
+        print "Cat, the board is full"
